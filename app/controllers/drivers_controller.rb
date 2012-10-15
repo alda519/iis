253289c3f236ed21545f35e8e46afdba86660332
alda519 <galda519@gmail.com>
2012-10-15 17:01:41 +0200

    plates done

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,32 +1,38 @@
 class DriversController < ApplicationController
 
-  def index
-    @drivers = Driver.all
-  end
+    def index
+        @drivers = Driver.all
+    end
 
-  def show
-    @driver = Driver.find(params[:id])
-  end
+    def show
+        @driver = Driver.find(params[:id])
+    end
 
-  def edit
-    @driver = Driver.find(params[:id])
-  end
+    def edit
+        @driver = Driver.find(params[:id])
+    end
 
-  def new
-    @driver = Driver.new
-  end
+    def new
+        @driver = Driver.new
+    end
 
-  def create
-    Driver.new(params[:driver]).save
-    redirect_to :action => :index
-  end
+    def create
+        Driver.new(params[:driver]).save
+        redirect_to :action => :index
+    end
 
-  def update
-  end
+    def update
+        @driver = Driver.find(params[:id])
+        if @driver.update_attributes(params[:driver])
+            redirect_to :action => :index
+        else
+            render :edit
+        end
+    end
 
-  def destroy
-    Driver.find(params[:id]).delete
-    redirect_to :action => :index
-  end
+    def destroy
+        Driver.find(params[:id]).delete
+        redirect_to :action => :index
+    end
 
 end
--- app/controllers/registration_plates_controller.rb
+++ app/controllers/registration_plates_controller.rb
@@ -1,13 +1,37 @@
 class RegistrationPlatesController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+    def index
+        @plates = RegistrationPlate.all
+    end
 
-  def new
-  end
+    def show
+        @plate = RegistrationPlate.find(params[:id])
+    end
 
-  def edit
-  end
+    def edit
+        @plate = RegistrationPlate.find(params[:id])
+    end
+
+    def new
+        @plate = RegistrationPlate.new
+    end
+
+    def create
+        RegistrationPlate.new(params[:registration_plate]).save
+        redirect_to :action => :index
+    end
+
+    def update
+        @plate = RegistrationPlate.find(params[:id])
+        if @plate.update_attributes(params[:registration_plate])
+            redirect_to :action => :index
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        RegistrationPlate.find(params[:id]).delete
+    end
+
 end
