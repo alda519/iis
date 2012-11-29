544aa5cff6881ad4e4cfdbbfcae02f846ed0d543
alda519 <galda519@gmail.com>
2012-11-29 10:12:38 +0100

    db migration fixed

--- db/migrate/20121128142536_removetimestampsfromjointable.rb
+++ db/migrate/20121128142536_removetimestampsfromjointable.rb
@@ -3,7 +3,7 @@ class Removetimestampsfromjointable < ActiveRecord::Migration
     def change
         change_table :roles_users do |t|
             t.remove :created_at
-            t.remove :uppdated_at
+            t.remove :updated_at
         end
     end
 
--- db/migrate/20121128143154_removealltimestampsfromjointable.rb
+++
@@ -1,9 +0,0 @@
-class Removealltimestampsfromjointable < ActiveRecord::Migration
-
-    def change
-        change_table :roles_users do |t|
-            t.remove :updated_at
-        end
-    end
-
-end
