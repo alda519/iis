ae05b33eb230ffec9646668da7a8a4078d70f861
alda519 <galda519@gmail.com>
2012-12-04 16:03:42 +0100

    foreign keys pridany, snad na heroku pujdou

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'haml'
 
 gem 'bcrypt-ruby'
 
+gem 'foreigner'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203120618) do
+ActiveRecord::Schema.define(:version => 20121203200000) do
 
   create_table "bonus_points", :force => true do |t|
     t.integer  "points"
